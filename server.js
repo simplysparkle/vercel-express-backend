53e9c531666220c2ec2220c6efb30bc1029754c7
ssbs <simplysparklewebsite@gmail.com>
2024-09-22 12:38:04 +0530

    2 commit

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const appointmentController = require('../controllers/appointmentController');
 
+
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'API is working' });
+  });   
+
 router.post('/', appointmentController.createAppointment);
 router.get('/', appointmentController.getAppointments);
 
